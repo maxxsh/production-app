8d6b714d8e393abb5a1107c48fe6b4c612d678d4
maxxsh <maxxshestov@gmail.com>
2022-09-26 16:38:14 -0400

    linters

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:i18next/recommended',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+    },
+  plugins: [
+    'react',
+        '@typescript-eslint',
+        'i18next',
+  ],
+  rules: {
+    'react/jsx-indent': [2, 4],
+    'react/jsx-indent-props': [2, 4],
+    // indent: [2, 4],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "@typescript-eslint/no-floating-promises": "off",
+    'i18next/no-literal-string': ['error', { markupOnly: true }],
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        "checksVoidReturn": false
+      }
+    ],
+},
+globals: {
+    __IS_DEV__: true,
+},
+}
