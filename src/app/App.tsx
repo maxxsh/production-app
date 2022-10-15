b97c88f0867a5fe6b952c65064e48777e5fa334c
maxxsh <maxxshestov@gmail.com>
2022-10-15 17:47:22 -0400

    loader, error boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
@@ -6,7 +6,7 @@ import { AppRouter } from 'app/providers/router'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
 
-const App = () => {
+function App () {
   const { theme } = useTheme()
 
   return (
