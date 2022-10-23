37706baf15836286c55e44bfc29ce2ded4ae75f1
maxxsh <maxxshestov@gmail.com>
2022-10-23 15:18:54 -0400

    login input

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,17 @@
-import { Counter } from 'entities/Counter'
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { Input } from 'shared/ui/Input/Input'
 
 const MainPage = () => {
   const { t } = useTranslation()
+  const [value, setValue] = useState('')
+
+  const onChange = (val: string) => {
+    setValue(val)
+  }
 
   return (
-
-      <div><Counter />
+      <div>
           {t('Главная страница')}
       </div>
   )
