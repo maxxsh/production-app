041c2f838c41d09ae84457ddfc507d2b50d19308
maxxsh <maxxshestov@gmail.com>
2022-11-06 19:14:19 -0500

    reducer manager fixed +

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,16 +1,21 @@
-import { configureStore, ReducersMapObject } from '@reduxjs/toolkit'
+import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit'
 import { counterReducer } from 'entities/Counter'
 import { userReducer } from 'entities/User'
-// import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice'
-import { createReducerManager } from './reducerManager'
 import { StateSchema } from './StateSchema'
+import { createReducerManager } from './reducerManager'
 
-export function createReduxStore (initialState?: StateSchema) {
+export function createReduxStore (
+  initialState?: StateSchema,
+  asyncReducers?: ReducersMapObject<StateSchema>
+) {
   const rootReducers: ReducersMapObject<StateSchema> = {
+    ...asyncReducers,
     counter: counterReducer,
     user: userReducer
   }
+
   const reducerManager = createReducerManager(rootReducers)
+
   const store = configureStore<StateSchema>({
     reducer: reducerManager.reduce,
     devTools: __IS_DEV__,
@@ -19,5 +24,6 @@ export function createReduxStore (initialState?: StateSchema) {
 
   // @ts-expect-error
   store.reducerManager = reducerManager
+
   return store
 }
