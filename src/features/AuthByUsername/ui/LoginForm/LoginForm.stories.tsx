c9198c5f1bc0f5a1815681169301549a59fc44c5
maxxsh <maxxshestov@gmail.com>
2022-11-06 19:00:48 -0500

    reduser manager

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import { LoginForm } from './LoginForm'
+import LoginForm from './LoginForm'
 
 export default {
   title: 'features/LoginForm',
