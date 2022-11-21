e47aafc990fdbddd17a7c3d245e062ed131bf2d9
maxxsh <maxxshestov@gmail.com>
2022-11-20 20:40:58 -0500

    memo profile

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,17 +1,19 @@
-import {
-  EnhancedStore,
-  AnyAction, Reducer, ReducersMapObject
-} from '@reduxjs/toolkit'
 import { CounterSchema } from 'entities/Counter'
 import { UserSchema } from 'entities/User'
 import { LoginSchema } from 'features/AuthByUsername'
+import {
+  AnyAction, EnhancedStore, Reducer, ReducersMapObject
+} from '@reduxjs/toolkit'
 import { CombinedState } from 'redux'
+import { ProfileSchema } from 'entities/Profile'
+
 export interface StateSchema {
   counter: CounterSchema
   user: UserSchema
 
   // Асинхронные редюсеры
   loginForm?: LoginSchema
+  profile?: ProfileSchema
 }
 
 export type StateSchemaKey = keyof StateSchema
@@ -22,6 +24,7 @@ export interface ReducerManager {
   add: (key: StateSchemaKey, reducer: Reducer) => void
   remove: (key: StateSchemaKey) => void
 }
+
 export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
   reducerManager: ReducerManager
 }
--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,10 +1,8 @@
-import { classNames } from 'shared/lib/classNames/classNames'
 import { Modal } from 'shared/ui/Modal/Modal'
-import cls from './LoginModal.module.scss'
-// import LoginForm from '../LoginForm/LoginForm'
+import { classNames } from 'shared/lib/classNames/classNames'
 import { Suspense } from 'react'
-import { LoginFormAsync } from '../LoginForm/LoginForm.async'
 import { Loader } from 'shared/ui/Loader/Loader'
+import { LoginFormAsync } from '../LoginForm/LoginForm.async'
 
 interface LoginModalProps {
   className?: string
@@ -14,14 +12,13 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
     <Modal
-        className={classNames(cls.LoginModal, {}, [className])}
+        className={classNames('', {}, [className])}
         isOpen={isOpen}
         onClose={onClose}
         lazy
     >
-        <Suspense fallback={<Loader/>}>
-            <LoginFormAsync />
+        <Suspense fallback={<Loader />}>
+            <LoginFormAsync onSuccess={onClose} />
         </Suspense>
-
     </Modal>
 )
